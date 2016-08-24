bfa08df032dbad44024a24ad4224ee531d687ccd
Ilya Chesnokov <me@ichesnokov.ru>
2016-08-24 18:37:31 +0300

    Save 2 lines on promises.pl

--- examples/promises.pl
+++ examples/promises.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/env perl
 use v5.12;
 use warnings;
+use experimental qw(signatures);
+
 use Promises qw(deferred);
 use AE;
 
 my @timers;
 my $cv = AE::cv;
 
-sub msgAfterTimeout {
-    my ($msg, $who, $timeout_ms) = @_;
-
+sub msgAfterTimeout($msg, $who, $timeout_ms) {
     my $deferred = deferred;
     $cv->begin;
     push @timers, AE::timer $timeout_ms / 1000, 0, sub {
@@ -19,11 +19,9 @@ sub msgAfterTimeout {
     $deferred->promise;
 }
 
-msgAfterTimeout("", "Foo", 500)->then(sub {
-    my $msg = shift;
+msgAfterTimeout("", "Foo", 500)->then(sub ($msg) {
     msgAfterTimeout($msg, 'Bar', 1000);
-})->then(sub {
-    my $msg = shift;
+})->then(sub ($msg) {
     say "Done after 1500ms:$msg";
 });
 $cv->recv;
